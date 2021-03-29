144d088ddcc74377113d684ddd752b3d6c9fefb9
Francesco <fha@m2mfactory.fr>
2021-03-29 15:56:31 +0200

    maj

--- modules/francesco.py
+++ modules/francesco.py
@@ -6,13 +6,21 @@ base64 = string.ascii_uppercase + string.ascii_lowercase + "123456789+/"
 
 def exercie9(list):
     list = [elem.to_bytes(5, byteorder='big').decode('utf-8') for elem in list]
-
     return list
 
 
 def int_to_base64(value):
     if base64 / value >= len(base64):
         print("hello")
+
+
+"""
+list : array of string
+
+translate list to string with all values in the list
+
+return string
+"""
 
 
 def exercie10(list):
@@ -22,10 +30,16 @@ def exercie10(list):
     return val
 
 
-def exercie11(string):
-    while len(string) % 4 != 0:
-        string = string+"="
-    return string
+"""
+value :  string
+
+add "=" while value is not multiple of 4
+
+return string
+"""
 
 
-exercie9([1, 2, 2])
+def exercie11(value):
+    while len(value) % 4 != 0:
+        value = value+"="
+    return value
