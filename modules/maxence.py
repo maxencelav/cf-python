91f0761f04fde79adb50e66a84c06b15d225b291
Maxence Lavenu <50020222+maxencelav@users.noreply.github.com>
2021-03-29 14:21:25 +0200

    New modules folder

---
+++ modules/maxence.py
@@ -0,0 +1,15 @@
+"""Module name
+
+Copyright (c) 2021 Maxence LAVENU
+All Rights Reserved.
+Released under the MIT license
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
